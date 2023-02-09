6d3449f7ab886776cfe1344c8f9a4a8b32cc7136
Pavel Svejda <pavlis.1@seznam.cz>
2023-02-09 23:56:12 +0100

    init

---
+++ src/com/engeto/promenne/Main.java
@@ -0,0 +1,14 @@
+package com.engeto.promenne;
+
+public class Main {
+    public static void main(String[] args) {
+        int delkaStranyA = 10;
+        int delkaStranyB = 5;
+
+        int plochaObdelniku;
+        plochaObdelniku = delkaStranyA * delkaStranyB;
+
+        System.out.println("Plocha obdelniku se stranami A: " + delkaStranyA + "m a B: " + delkaStranyB + "m");
+        System.out.println("je: " + plochaObdelniku + "m^2.");
+    }
+}
